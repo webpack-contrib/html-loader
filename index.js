220527cca9564c8890bbbeecf2f3825dd12bcfeb
Gajus Kuizinas <gk@anuary.com>
2015-07-27 11:55:44 +0100

    Added eslint, editorconfig, standardised syntax style

--- index.js
+++ index.js
@@ -1,75 +1,110 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
-var htmlMinifier = require("html-minifier");
-var attrParse = require("./lib/attributesParser");
-var SourceNode = require("source-map").SourceNode;
-var loaderUtils = require("loader-utils");
-var url = require("url");
+var htmlMinifier = require('html-minifier'),
+    attrParse = require('./lib/attributesParser'),
+    loaderUtils = require('loader-utils'),
+    url = require('url'),
+    randomIdent;
 
-function randomIdent() {
-	return "xxxHTMLLINKxxx" + Math.random() + Math.random() + "xxx";
+randomIdent = function () {
+    var id = ++randomIdent.id;
+
+    return '___HTML_LINK___' + id + '___';
 };
 
+randomIdent.id = 0;
 
-module.exports = function(content) {
-	this.cacheable && this.cacheable();
-	var query = loaderUtils.parseQuery(this.query);
-	var attributes = ["img:src"];
-	if(query.attrs !== undefined) {
-		if(typeof query.attrs === "string")
-			attributes = query.attrs.split(" ");
-		else if(Array.isArray(query.attrs))
-			attributes = query.attrs;
-		else if(query.attrs === false)
-			attributes = [];
-		else
-			throw new Error("Invalid value to query parameter attrs");
-	}
-	var root = query.root;
-	var links = attrParse(content, function(tag, attr) {
-		return attributes.indexOf(tag + ":" + attr) >= 0;
-	});
-	links.reverse();
-	var data = {};
-	content = [content];
-	links.forEach(function(link) {
-		if(!loaderUtils.isUrlRequest(link.value, root)) return;
-        
-		var uri = url.parse(link.value);
-		if (uri.hash !== null && uri.hash !== undefined) {
-		    uri.hash = null;
-		    link.value = uri.format();
-		    link.length = link.value.length;
-		}
+module.exports = function (content) {
+    var query = loaderUtils.parseQuery(this.query),
+        attributes = ['img:src'],
+        root,
+        links,
+        data,
+        minimize;
 
-        
-		do {
-			var ident = randomIdent();
-		} while(data[ident]);
-		data[ident] = link.value;
-		var x = content.pop();
-		content.push(x.substr(link.start + link.length));
-		content.push(ident);
-		content.push(x.substr(0, link.start));
-	});
-	content.reverse();
-	content = content.join("");
-	if(typeof query.minimize === "boolean" ? query.minimize : this.minimize) {
-		content = htmlMinifier.minify(content, {
-			removeComments: query.removeComments !== false,
-			collapseWhitespace: query.collapseWhitespace !== false,
-			collapseBooleanAttributes: query.collapseBooleanAttributes !== false,
-			removeAttributeQuotes: query.removeAttributeQuotes !== false,
-			removeRedundantAttributes: query.removeRedundantAttributes !== false,
-			useShortDoctype: query.useShortDoctype !== false,
-			removeEmptyAttributes: query.removeEmptyAttributes !== false,
-			removeOptionalTags: query.removeOptionalTags !== false
-		});
-	}
-	return "module.exports = " + JSON.stringify(content).replace(/xxxHTMLLINKxxx[0-9\.]+xxx/g, function(match) {
-		if(!data[match]) return match;
-		return '" + require(' + JSON.stringify(loaderUtils.urlToRequest(data[match], root)) + ') + "';
-	}) + ";";
-}
+    if (this.cacheable) {
+        this.cacheable();
+    }
+
+    if (query.attrs !== undefined) {
+        if (typeof query.attrs === 'string') {
+            attributes = query.attrs.split(' ');
+        } else if (Array.isArray(query.attrs)) {
+            attributes = query.attrs;
+        } else if (query.attrs === false) {
+            attributes = [];
+        } else {
+            throw new Error('Invalid value to query parameter attrs');
+        }
+    }
+    root = query.root;
+    links = attrParse(content, function (tag, attr) {
+        return attributes.indexOf(tag + ':' + attr) >= 0;
+    });
+    links.reverse();
+    data = {};
+    content = [content];
+
+    links.forEach(function (link) {
+        var uri,
+            x,
+            ident;
+
+        if (!loaderUtils.isUrlRequest(link.value, root)) {
+            return;
+        }
+
+        uri = url.parse(link.value);
+
+        if (uri.hash !== null && uri.hash !== undefined) {
+            uri.hash = null;
+            link.value = uri.format();
+            link.length = link.value.length;
+        }
+
+        do {
+            ident = randomIdent();
+        } while (data[ident]);
+
+        data[ident] = link.value;
+
+        x = content.pop();
+
+        content.push(x.substr(link.start + link.length));
+        content.push(ident);
+        content.push(x.substr(0, link.start));
+    });
+
+    content.reverse();
+    content = content.join('');
+
+    if (typeof query.minimize === 'boolean') {
+        minimize = query.minimize;
+    } else {
+        // @see // --optimize-minimize
+        minimize = this.minimize;
+    }
+
+    if (minimize) {
+        content = htmlMinifier.minify(content, {
+            removeComments: query.removeComments !== false,
+            collapseWhitespace: query.collapseWhitespace !== false,
+            collapseBooleanAttributes: query.collapseBooleanAttributes !== false,
+            removeAttributeQuotes: query.removeAttributeQuotes !== false,
+            removeRedundantAttributes: query.removeRedundantAttributes !== false,
+            useShortDoctype: query.useShortDoctype !== false,
+            removeEmptyAttributes: query.removeEmptyAttributes !== false,
+            removeOptionalTags: query.removeOptionalTags !== false
+        });
+    }
+
+    content = JSON.stringify(content);
+
+    content = content.replace(/___HTML_LINK___[0-9\.]+___/g, function (match) {
+        if (!data[match]) {
+            throw new Error('HTML link cannot be resolved.');
+        }
+
+        return '" + require(' + JSON.stringify(loaderUtils.urlToRequest(data[match], root)) + ') + "';
+    });
+
+    return 'module.exports = ' + content + ';';
+};
--- test/loaderTest.js
+++ test/loaderTest.js
@@ -1,61 +1,59 @@
-var should = require("should");
+var loader = require('../');
 
-var loader = require("../");
-
-describe("loader", function() {
-	it("should convert to requires", function() {
-		loader.call({}, 'Text <img src="image.png"><img src="~bootstrap-img"> Text').should.be.eql(
-			'module.exports = "Text <img src=\\"" + require("./image.png") + "\\"><img src=\\"" + require("bootstrap-img") + "\\"> Text";'
-		);
-	});
-	it("should accept attrs from query", function() {
-		loader.call({
-			query: "?attrs=script:src"
-		}, 'Text <script src="script.js"><img src="image.png">').should.be.eql(
-			'module.exports = "Text <script src=\\"" + require("./script.js") + "\\"><img src=\\"image.png\\">";'
-		);
-	});
-	it("should accept attrs from query (space separated)", function() {
-		loader.call({
-			query: "?attrs=script:src img:src"
-		}, 'Text <script src="script.js"><img src="image.png">').should.be.eql(
-			'module.exports = "Text <script src=\\"" + require("./script.js") + "\\"><img src=\\"" + require("./image.png") + "\\">";'
-		);
-	});
-	it("should accept attrs from query (multiple)", function() {
-		loader.call({
-			query: "?attrs[]=script:src&attrs[]=img:src"
-		}, 'Text <script src="script.js"><img src="image.png">').should.be.eql(
-			'module.exports = "Text <script src=\\"" + require("./script.js") + "\\"><img src=\\"" + require("./image.png") + "\\">";'
-		);
-	});
-	it("should not make bad things with templates", function() {
-		loader.call({}, '<h3>#{number} {customer}</h3>\n<p>   {title}   </p>').should.be.eql(
-			'module.exports = "<h3>#{number} {customer}</h3>\\n<p>   {title}   </p>";'
-		);
-	});
-	it("should minimize", function() {
-		loader.call({
-			minimize: true
-		}, '<!-- comment --><h3>#{number} {customer}</h3>\n<p>   {title}   </p>\n\t <!-- comment --> <img src="image.png" />').should.be.eql(
-			'module.exports = "<h3>#{number} {customer}</h3><p>{title}</p><img src=" + require("./image.png") + ">";'
-		);
-	});
-	it("should not translate root-relative urls (without root query)", function() {
-		loader.call({}, 'Text <img src="/image.png">').should.be.eql(
-			'module.exports = "Text <img src=\\"/image.png\\">";'
-		);
-	});
-	it("should accept root from query", function() {
-		loader.call({
-			query: "?root=/test"
-		}, 'Text <img src="/image.png">').should.be.eql(
-			'module.exports = "Text <img src=\\"" + require("/test/image.png") + "\\">";'
-		);
-	});
-	it("should ignore hash fragments in URLs", function() {
-	    loader.call({}, '<img src="icons.svg#hash">').should.be.eql(
-	        'module.exports = "<img src=\\"" + require("./icons.svg") + "#hash\\">";'
-	    );
-	});
+describe('loader', function () {
+    it('should convert to requires', function() {
+        loader.call({}, 'Text <img src="image.png"><img src="~bootstrap-img"> Text').should.be.eql(
+            'module.exports = "Text <img src=\\"" + require("./image.png") + "\\"><img src=\\"" + require("bootstrap-img") + "\\"> Text";'
+        );
+    });
+    it('should accept attrs from query', function () {
+        loader.call({
+            query: '?attrs=script:src'
+        }, 'Text <script src="script.js"><img src="image.png">').should.be.eql(
+            'module.exports = "Text <script src=\\"" + require("./script.js") + "\\"><img src=\\"image.png\\">";'
+        );
+    });
+    it('should accept attrs from query (space separated)', function () {
+        loader.call({
+            query: '?attrs=script:src img:src'
+        }, 'Text <script src="script.js"><img src="image.png">').should.be.eql(
+            'module.exports = "Text <script src=\\"" + require("./script.js") + "\\"><img src=\\"" + require("./image.png") + "\\">";'
+        );
+    });
+    it('should accept attrs from query (multiple)', function () {
+        loader.call({
+            query: '?attrs[]=script:src&attrs[]=img:src'
+        }, 'Text <script src="script.js"><img src="image.png">').should.be.eql(
+            'module.exports = "Text <script src=\\"" + require("./script.js") + "\\"><img src=\\"" + require("./image.png") + "\\">";'
+        );
+    });
+    it('should not make bad things with templates', function () {
+        loader.call({}, '<h3>#{number} {customer}</h3>\n<p>   {title}   </p>').should.be.eql(
+            'module.exports = "<h3>#{number} {customer}</h3>\\n<p>   {title}   </p>";'
+        );
+    });
+    it('should minimize', function () {
+        loader.call({
+            minimize: true
+        }, '<!-- comment --><h3>#{number} {customer}</h3>\n<p>   {title}   </p>\n\t <!-- comment --> <img src="image.png" />').should.be.eql(
+            'module.exports = "<h3>#{number} {customer}</h3><p>{title}</p><img src=\\"" + require("./image.png") + "\\">";'
+        );
+    });
+    it('should not translate root-relative urls (without root query)', function () {
+        loader.call({}, 'Text <img src="/image.png">').should.be.eql(
+            'module.exports = "Text <img src=\\"/image.png\\">";'
+        );
+    });
+    it('should accept root from query', function () {
+        loader.call({
+            query: '?root=/test'
+        }, 'Text <img src="/image.png">').should.be.eql(
+            'module.exports = "Text <img src=\\"" + require("/test/image.png") + "\\">";'
+        );
+    });
+    it('should ignore hash fragments in URLs', function () {
+        loader.call({}, '<img src="icons.svg#hash">').should.be.eql(
+            'module.exports = "<img src=\\"" + require("./icons.svg") + "#hash\\">";'
+        );
+    });
 });
