webpack-contrib/html-loader

commit 70370dc369d40bc14958b2c8fce298ab1b78eb8a

feat: add support for empty tags in `tag:attribute` matching (#129)

index.js
@@ -39,7 +39,11 @@ module.exports = function(content) {
 	}
 	var root = config.root;
 	var links = attrParse(content, function(tag, attr) {
-		return attributes.indexOf(tag + ":" + attr) >= 0;
+		var item = tag + ":" + attr;
+		var res = attributes.find(function(a) {
+		  return item.indexOf(a) >= 0;
+		});
+		return !!res;
 	});
 	links.reverse();
 	var data = {};

test/loaderTest.js
@@ -29,6 +29,13 @@ describe("loader", function() {
 			'module.exports = "Text <script src=\\"" + require("./script.js") + "\\"><img src=\\"" + require("./image.png") + "\\">";'
 		);
 	});
+	it("should accept :attribute (empty tag) from query", function() {
+		loader.call({
+			query: "?attrs[]=:custom-src"
+		}, 'Text <custom-element custom-src="image1.png"><custom-img custom-src="image2.png"/></custom-element>').should.be.eql(
+			'module.exports = "Text <custom-element custom-src=\\"" + require("./image1.png") + "\\"><custom-img custom-src=\\"" + require("./image2.png") + "\\"/></custom-element>";'
+		);
+	});
 	it("should not make bad things with templates", function() {
 		loader.call({}, '<h3>#{number} {customer}</h3>\n<p>   {title}   </p>').should.be.eql(
 			'module.exports = "<h3>#{number} {customer}</h3>\\n<p>   {title}   </p>";'

commit 6efa6de3152f10d2eb360c7e520e6ec3a81503cf

fix: Support for empty tags in tag-attribute matching (#133)
Fix a bug introduced with #129.

index.js
@@ -39,9 +39,12 @@ module.exports = function(content) {
 	}
 	var root = config.root;
 	var links = attrParse(content, function(tag, attr) {
-		var item = tag + ":" + attr;
 		var res = attributes.find(function(a) {
-		  return item.indexOf(a) >= 0;
+			if (a.charAt(0) === ':') {
+				return attr === a.slice(1);
+			} else {
+				return (tag + ":" + attr) === a;
+			}
 		});
 		return !!res;
 	});

test/loaderTest.js
@@ -36,6 +36,13 @@ describe("loader", function() {
 			'module.exports = "Text <custom-element custom-src=\\"" + require("./image1.png") + "\\"><custom-img custom-src=\\"" + require("./image2.png") + "\\"/></custom-element>";'
 		);
 	});
+	it("should accept :attribute (empty tag) from query and not collide with similar attributes", function() {
+		loader.call({
+			query: "?attrs[]=:custom-src"
+		}, 'Text <custom-element custom-src="image1.png" custom-src-other="other.png"><custom-img custom-src="image2.png"/></custom-element>').should.be.eql(
+			'module.exports = "Text <custom-element custom-src=\\"" + require("./image1.png") + "\\" custom-src-other=\\"other.png\\"><custom-img custom-src=\\"" + require("./image2.png") + "\\"/></custom-element>";'
+		);
+	});
 	it("should not make bad things with templates", function() {
 		loader.call({}, '<h3>#{number} {customer}</h3>\n<p>   {title}   </p>').should.be.eql(
 			'module.exports = "<h3>#{number} {customer}</h3>\\n<p>   {title}   </p>";'