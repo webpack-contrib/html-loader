2987e37f6069c715de25c3df30f0fba7ebaa0264
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2018-01-08 02:46:34 +0100

    feat(plugins/import): add `<import>` filter support (`options.import`) (#163)

--- test/options/import.test.js
+++ test/options/import.test.js
@@ -3,7 +3,7 @@ import webpack from '../helpers/compiler';
 
 describe('Options', () => {
   describe('import', () => {
-    test('{Boolean}', async () => {
+    test('{Boolean} - true - default', async () => {
       const config = {
         loader: {
           test: /\.html$/,
@@ -12,6 +12,56 @@ describe('Options', () => {
       };
 
       const stats = await webpack('options/import/fixture.js', config);
+      const { source } = stats.toJson().modules[1];
+
+      expect(source).toMatchSnapshot();
+    });
+
+    test('{Boolean} - false', async () => {
+      const config = {
+        loader: {
+          test: /\.html$/,
+          options: {
+            import: false
+          },
+        },
+      };
+
+      const stats = await webpack('options/import/fixture.js', config);
+      const { source } = stats.toJson().modules[1];
+
+      expect(source).toMatchSnapshot();
+    });
+
+    test('{RegExp}', async () => {
+      const config = {
+        loader: {
+          test: /\.html$/,
+          options: {
+            import: /filter/
+          },
+        },
+      };
+
+      const stats = await webpack('options/import/filter/fixture.js', config);
+      const { source } = stats.toJson().modules[1];
+
+      expect(source).toMatchSnapshot();
+    });
+
+    test('{Function}', async () => {
+      const config = {
+        loader: {
+          test: /\.html$/,
+          options: {
+            import (url) {
+              return /filter/.test(url)
+            }
+          },
+        },
+      };
+
+      const stats = await webpack('options/import/filter/fixture.js', config);
       const { source } = stats.toJson().modules[1];
 
       expect(source).toMatchSnapshot();
