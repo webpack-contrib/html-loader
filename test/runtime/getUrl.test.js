3d029c136f7df7f4c52464e0d4cc062efe85ab08
evilebottnawi <sheo13666q@gmail.com>
2020-01-21 17:28:09 +0300

    refactor: code

--- test/runtime/getUrl.test.js
+++ test/runtime/getUrl.test.js
@@ -7,6 +7,17 @@ describe('getUrl', () => {
     // eslint-disable-next-line no-undefined
     expect(getUrl(undefined)).toMatchSnapshot();
     expect(getUrl('image.png')).toMatchSnapshot();
+    expect(getUrl('image\timage.png')).toMatchSnapshot();
+    expect(getUrl('image\nimage.png')).toMatchSnapshot();
+    expect(getUrl('image\fimage.png')).toMatchSnapshot();
+    expect(getUrl('image\rimage.png')).toMatchSnapshot();
+    expect(getUrl('image image.png')).toMatchSnapshot();
+    expect(getUrl('image"image.png')).toMatchSnapshot();
+    expect(getUrl("image'image.png")).toMatchSnapshot();
+    expect(getUrl('image=image.png')).toMatchSnapshot();
+    expect(getUrl('image>image.png')).toMatchSnapshot();
+    expect(getUrl('image<image.png')).toMatchSnapshot();
+    expect(getUrl('image`image.png')).toMatchSnapshot();
     expect(
       getUrl({ default: 'image.png', __esModule: true })
     ).toMatchSnapshot();
