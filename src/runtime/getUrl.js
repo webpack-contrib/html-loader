631a1539456912ed224e505ab2fdd6ab6704117e
evilebottnawi <sheo13666q@gmail.com>
2020-01-21 16:01:01 +0300

    refactor: code

---
+++ src/runtime/getUrl.js
@@ -0,0 +1,10 @@
+module.exports = (url) => {
+  // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+  url = url && url.__esModule ? url.default : url;
+
+  if (typeof url !== 'string') {
+    return url;
+  }
+
+  return url;
+};
