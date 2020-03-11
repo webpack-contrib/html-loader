a17df4937e3e8ab7216647a04cab8ce4dd3bd6f6
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2020-03-11 21:17:57 +0300

    fix: reduce `import`/`require` count

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { getOptions } from 'loader-utils';
 import validateOptions from 'schema-utils';
 
-import { attributePlugin, interpolatePlugin, minimizerPlugin } from './plugins';
+import { sourcePlugin, interpolatePlugin, minimizerPlugin } from './plugins';
 import Warning from './Warning';
 
 import {
   pluginRunner,
   isProductionMode,
   getImportCode,
+  getModuleCode,
   getExportCode,
 } from './utils';
 
@@ -27,7 +28,7 @@ export default function htmlLoader(content) {
     typeof options.attributes === 'undefined' ? true : options.attributes;
 
   if (attributes) {
-    plugins.push(attributePlugin(options));
+    plugins.push(sourcePlugin(options));
   }
 
   const minimize =
@@ -57,19 +58,29 @@ export default function htmlLoader(content) {
     this.emitError(new Error(error));
   }
 
-  const replacers = [];
+  const importedMessages = [];
+  const replaceableMessages = [];
+  const exportedMessages = [];
 
   for (const message of messages) {
     // eslint-disable-next-line default-case
     switch (message.type) {
+      case 'import':
+        importedMessages.push(message.value);
+        break;
       case 'replacer':
-        replacers.push(message.value);
+        replaceableMessages.push(message.value);
+        break;
+      case 'exports':
+        exportedMessages.push(message.value);
         break;
     }
   }
 
-  const importCode = getImportCode(this, html, replacers, options);
-  const exportCode = getExportCode(html, replacers, options);
+  const codeOptions = { ...options, loaderContext: this };
+  const importCode = getImportCode(html, importedMessages, codeOptions);
+  const moduleCode = getModuleCode(html, replaceableMessages, codeOptions);
+  const exportCode = getExportCode(html, exportedMessages, codeOptions);
 
-  return `${importCode}${exportCode};`;
+  return `${importCode}${moduleCode}${exportCode}`;
 }
