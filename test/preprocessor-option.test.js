586b15a3a838e5d4ddda28d98a7dfbd70df3c609
Islam ElHakmi <eslam.elhakmey3@gmail.com>
2020-03-30 15:42:12 +0200

    feat: add support for async functions to preprocessor option

--- test/preprocessor-option.test.js
+++ test/preprocessor-option.test.js
@@ -47,6 +47,40 @@ describe("'process' option", () => {
     expect(getErrors(stats)).toMatchSnapshot('errors');
   });
 
+  it('should work with Async "preprocessor" Function option', async () => {
+    const compiler = getCompiler('preprocessor.hbs', {
+      preprocessor: async (content, loaderContext) => {
+        await expect(typeof content).toBe('string');
+        await expect(loaderContext).toBeDefined();
+
+        let result;
+
+        try {
+          result = await Handlebars.compile(content)({
+            firstname: 'Alexander',
+            lastname: 'Krasnoyarov',
+          });
+        } catch (error) {
+          await loaderContext.emitError(error);
+
+          return content;
+        }
+
+        return result;
+      },
+    });
+    const stats = await compile(compiler);
+
+    expect(getModuleSource('./preprocessor.hbs', stats)).toMatchSnapshot(
+      'module'
+    );
+    expect(
+      execute(readAsset('main.bundle.js', compiler, stats))
+    ).toMatchSnapshot('result');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
+  });
+
   it('should work with the "preprocessor" option #2', async () => {
     const plugin = posthtmlWebp();
     const compiler = getCompiler('posthtml.html', {
@@ -58,6 +92,37 @@ describe("'process' option", () => {
 
         try {
           result = posthtml()
+            .use(plugin)
+            .process(content, { sync: true });
+        } catch (error) {
+          loaderContext.emitError(error);
+
+          return content;
+        }
+
+        return result.html;
+      },
+    });
+    const stats = await compile(compiler);
+
+    expect(getModuleSource('./posthtml.html', stats)).toMatchSnapshot('module');
+    expect(
+      execute(readAsset('main.bundle.js', compiler, stats))
+    ).toMatchSnapshot('result');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
+  });
+  it('should work with the Async "preprocessor" Function option #2', async () => {
+    const plugin = posthtmlWebp();
+    const compiler = getCompiler('posthtml.html', {
+      preprocessor: async (content, loaderContext) => {
+        await expect(typeof content).toBe('string');
+        await expect(loaderContext).toBeDefined();
+
+        let result;
+
+        try {
+          result = await posthtml()
             .use(plugin)
             .process(content, { sync: true });
         } catch (error) {
