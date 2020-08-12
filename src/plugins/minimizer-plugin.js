21d215a02a1cdbac498ad745cab68046475832dc
evilebottnawi <sheo13666q@gmail.com>
2020-08-12 19:13:36 +0300

    refactor: code

--- src/plugins/minimizer-plugin.js
+++ src/plugins/minimizer-plugin.js
@@ -2,32 +2,9 @@ import { minify } from 'html-minifier-terser';
 
 export default (options) =>
   function process(html) {
-    const minimizeOptions =
-      typeof options.minimize === 'boolean' ||
-      typeof options.minimize === 'undefined'
-        ? {
-            caseSensitive: true,
-            // `collapseBooleanAttributes` is not always safe, since this can break CSS attribute selectors and not safe for XHTML
-            collapseWhitespace: true,
-            conservativeCollapse: true,
-            keepClosingSlash: true,
-            // We need ability to use cssnano, or setup own function without extra dependencies
-            minifyCSS: true,
-            minifyJS: true,
-            // `minifyURLs` is unsafe, because we can't guarantee what the base URL is
-            // `removeAttributeQuotes` is not safe in some rare cases, also HTML spec recommends against doing this
-            removeComments: true,
-            // `removeEmptyAttributes` is not safe, can affect certain style or script behavior
-            removeRedundantAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            // `useShortDoctype` is not safe for XHTML
-          }
-        : options.minimize;
-
     try {
       // eslint-disable-next-line no-param-reassign
-      html = minify(html, minimizeOptions);
+      html = minify(html, options.minimize);
     } catch (error) {
       options.errors.push(error);
     }
