0fc24703a5fece78718fdc6b448c06caeee45754
cap-Bernardito <cap-Bernardito@yandex.ru>
2021-02-11 17:45:42 +0300

    feat: sources import with new URL

--- src/runtime/getUrl.js
+++ src/runtime/getUrl.js
@@ -11,10 +11,6 @@ module.exports = (url, options) => {
   // eslint-disable-next-line no-underscore-dangle, no-param-reassign
   url = String(url.__esModule ? url.default : url);
 
-  if (typeof url !== 'string') {
-    return url;
-  }
-
   if (options.hash) {
     // eslint-disable-next-line no-param-reassign
     url += options.hash;
