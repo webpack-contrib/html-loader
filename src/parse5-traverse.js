0758b1a032bec700d2c6f19af00379d6856a9305
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2021-02-19 18:14:27 +0300

    refactor: removed `parse5-sax-parser` package in favor `parse5` (#365)

---
+++ src/parse5-traverse.js
@@ -0,0 +1,26 @@
+const traverse = (root, callback) => {
+  const visit = (node, parent) => {
+    let res;
+
+    if (callback) {
+      res = callback(node, parent);
+    }
+
+    let { childNodes } = node;
+
+    // in case a <template> tag is in the middle of the HTML: https://github.com/JPeer264/node-rcs-core/issues/58
+    if (node.content && Array.isArray(node.content.childNodes)) {
+      ({ childNodes } = node.content);
+    }
+
+    if (res !== false && Array.isArray(childNodes) && childNodes.length >= 0) {
+      childNodes.forEach((child) => {
+        visit(child, node);
+      });
+    }
+  };
+
+  visit(root, null);
+};
+
+module.exports = traverse;
