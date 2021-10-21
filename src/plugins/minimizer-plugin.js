a2fedcc07c715876b877b32043ffa93b9489f966
evilebottnawi <sheo13666q@gmail.com>
2021-10-21 18:38:14 +0300

    feat: update minimizer

--- src/plugins/minimizer-plugin.js
+++ src/plugins/minimizer-plugin.js
@@ -1,10 +1,10 @@
 import { minify } from "html-minifier-terser";
 
 export default (options) =>
-  function process(html) {
+  async function process(html) {
     try {
       // eslint-disable-next-line no-param-reassign
-      html = minify(html, options.minimize);
+      html = await minify(html, options.minimize);
     } catch (error) {
       options.errors.push(error);
     }
